d5402542982b6c65b3b2076c05605adf8c697c8e
doldam42 <doldam42@github.com>
2022-11-25 12:21:57 +0900

    init commit

---
+++ src/main/java/doldam/teamChat/TeamChatApplication.java
@@ -0,0 +1,13 @@
+package doldam.teamChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamChatApplication.class, args);
+	}
+
+}
