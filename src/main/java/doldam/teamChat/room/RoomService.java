2a13b59cbffa9f99997d8399714d8a92d8840b46
doldam42 <doldam42@github.com>
2022-12-01 13:59:49 +0900

    RoomService enter 메서드 리펙토링

--- src/main/java/doldam/teamChat/room/RoomService.java
+++ src/main/java/doldam/teamChat/room/RoomService.java
@@ -29,13 +29,6 @@ public class RoomService {
         return new ArrayList<>(chatRooms.values());
     }
 
-    public Optional<RoomDTO> findActiveRoomById(String roomId) {
-        if (chatRooms.containsKey(roomId)) {
-            return Optional.of(chatRooms.get(roomId));
-        }
-        return Optional.empty();
-    }
-
     public RoomDTO createRoom(String title) {
         String randomId = UUID.randomUUID().toString();
         RoomDTO chatRoom = RoomDTO.builder()
@@ -51,21 +44,20 @@ public class RoomService {
      * 메모리에 방이 존재하지 않지만 데이터베이스 상에 존재할 경우 chatRoom 생성
      */
     public RoomDTO enter(String roomId) {
-        Optional<RoomDTO> op1 = findActiveRoomById(roomId);
-        // 메모리상에 채팅방이 존재하지 않으면 db에 방이 존재하는지 찾은 후 반환
-        if (op1.isEmpty()) {
-            Optional<Room> op2 = roomRepository.findById(roomId);
-            if (op2.isEmpty()) {
-                throw new NoSuchElementException();
-            }
-            RoomDTO chatRoom = RoomDTO.builder()
-                    .roomId(roomId)
-                    .title(op2.get().getTitle())
-                    .build();
-            chatRooms.put(roomId, chatRoom);
-            return chatRoom;
-        } else {
-            return op1.get();
+        // 메모리상에 채팅방이 존재하면 반환
+        if (chatRooms.containsKey(roomId)) {
+            return chatRooms.get(roomId);
         }
+        // 메모리상에 채팅방이 존재하지 않으면 db에 방이 존재하는지 찾은 후 반환
+        Optional<Room> op = roomRepository.findById(roomId);
+        if (op.isEmpty()) {
+            throw new NoSuchElementException("채팅방이 존재하지 않습니다.");
+        }
+        RoomDTO chatRoom = RoomDTO.builder()
+                .roomId(roomId)
+                .title(op.get().getTitle())
+                .build();
+        chatRooms.put(roomId, chatRoom);
+        return chatRoom;
     }
 }
